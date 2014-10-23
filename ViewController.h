15c76b82771b9149bc50243e13cd51245951b29f
mcguirephil01 <mcguirephil01@gmail.com>
2014-10-23 15:19:32 -0400

    Initial Commit
    Initial Commit

---
+++ ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PlayNice
+//
+//  Created by McGuire, Philip on 2/18/14.
+//  Copyright (c) 2014 McGuire, Philip. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PlayNice
+//
+//  Created by McGuire, Philip on 2/18/14.
+//  Copyright (c) 2014 McGuire, Philip. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
